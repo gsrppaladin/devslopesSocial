279eb4a0a57edbd6a11afc083660ad747ebf111c
Sam Greenhill <samgreenhill@Sams-MBP.home>
2017-01-07 16:26:01 -0500

    trying to get facebook authorization

--- Podfile
+++ Podfile
@@ -10,5 +10,8 @@ target 'devslopeSocial' do
   pod 'Firebase/Auth'
   pod 'Firebase/Database'
   pod 'Firebase/Storage'
-
+  pod 'FBSDKLoginKit'
+  pod 'FacebookCore'
+  pod 'FacebookLogin'
+  pod 'FacebookShare'
 end
--- devslopeSocial/SignInVC.swift
+++ devslopeSocial/SignInVC.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
+import FacebookLogin
+import FacebookCore
 
 class SignInVC: UIViewController {
+    
 
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-    }
+        
+        
+           }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
+    
+    
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("SAM: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("SAM: User cancelled Facebook authentication")
+            } else {
+                print("SAM: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("SAM: Unable to authenticate with firebase - \(error)")
+            } else {
+                print("SAM: Succesfully authenticated with firebase")
+                
+            }
+        })
+        
+    }
+    
+    
+    
+    
+    
+    
+    
 
+
+    
 
 }
 
